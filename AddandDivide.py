736b4e712ad1776058ded77a66895e019ea01d77
Ramasahayam Sreekaran Reddy <86601811+Sreekaran1704@users.noreply.github.com>
2021-09-29 19:42:05 +0530

    Add files via upload

---
+++ AddandDivide.py
@@ -0,0 +1,9 @@
+import math
+for i in range(int(input())):
+    a,b=map(int,input().split())
+    c=math.gcd(a,b)
+    d=a//c
+    if pow(b,d,a)==0:
+        print("YES")
+    else:
+        print("NO")
