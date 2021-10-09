80a923bd107e177333925bb3d95e4bfe6e292378
Ramasahayam Sreekaran Reddy <86601811+Sreekaran1704@users.noreply.github.com>
2021-10-09 18:29:36 +0530

    Create ThreeBoxes.py

---
+++ ThreeBoxes.py
@@ -0,0 +1,10 @@
+#Question Code : THREEBOX
+#code
+for i in range(int(input())):
+    a,b,c,d=map(int,input().split())
+    if (a + b + c <= d):
+        print(1)
+    elif (a + b <= d):
+        print(2)
+    else:
+        print(3)
