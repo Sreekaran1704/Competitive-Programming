07d63ba63c7fb5d78cbb3bd9aeca19fcbcf4e714
Ramasahayam Sreekaran Reddy <86601811+Sreekaran1704@users.noreply.github.com>
2021-10-20 20:40:51 +0530

    Create Q3.py

---
+++ Starters16/Q3.py
@@ -0,0 +1,9 @@
+#Question code - DIRECTN
+# cook your dish here
+for i in range(int(input())):
+    size=int(input())
+    s=input()
+    if "RR" in s or "LL" in s:
+        print("YES")
+    else:
+        print("NO")
