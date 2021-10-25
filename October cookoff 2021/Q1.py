b9bcd79c19ddec3e3e3b924631c8ab7883a47b8f
Ramasahayam Sreekaran Reddy <86601811+Sreekaran1704@users.noreply.github.com>
2021-10-25 21:08:26 +0530

    Added new file

---
+++ October cookoff 2021/Q1.py
@@ -0,0 +1,13 @@
+import sys
+input=sys.stdin.readline
+for i in range(int(input())):
+    n=int(input())
+    p=n
+    if p<=1:
+        sys.stdout.write(str(p)+"\n")
+    elif p==2:
+        sys.stdout.write(str(1)+"\n")
+    elif p==3:
+        sys.stdout.write(str(3)+"\n")
+    elif p>3:
+        sys.stdout.write(str(1+(n-1)*(n-2))+"\n")
